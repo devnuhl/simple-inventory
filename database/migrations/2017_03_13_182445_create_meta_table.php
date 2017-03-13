bb86cc390e466be6a0cd691b17b96edeb35d0457
Chris C <devnuhl@gmail.com>
2017-03-13 14:07:45 -0500

    use sqlite as default. add migrations

---
+++ database/migrations/2017_03_13_182445_create_meta_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('label', 128);
+            $table->char('value', 256);
+            $table->integer('item_id');
+            $table->foreign('item_id')->references('id')->on('items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meta');
+    }
+}
