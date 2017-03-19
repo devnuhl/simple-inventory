f1d0c802e470ade0b2ec3ba175ec2a08a3ab7705
Chris C <devnuhl@gmail.com>
2017-03-18 19:44:19 -0500

    add meta fields to form. currently being ignored.

--- resources/views/form/item.blade.php
+++ resources/views/form/item.blade.php
@@ -1,17 +1,28 @@
 @extends('main')
 
 @section('content')
+    <div>
     {!! Form::open() !!}
     @if (isset($item))
         @php
         Form::setModel($item)
         @endphp
     @endif
+        <p>
     {!! Form::hidden('id', isset($item) ? $item->id : '') !!}
-    {!! Form::text('label') !!}
-    {!! Form::text('description') !!}
+    {!! Form::text('label', null, ['placeholder' => 'Item Label']) !!}
+    {!! Form::text('description', null, ['placeholder' => 'Item Description']) !!}
     {!! Form::select('container_id', $options, (Route::input('container') ?: $item->container_id)) !!}
-
-    {!! Form::submit('Add Item') !!}
+        </p>
+        <p>
+            {!! Form::hidden('meta_id', isset($meta) ? $meta->id : '') !!}
+            {!! Form::hidden('item_id') !!}
+            {!! Form::text('meta_label', null, ['placeholder' => 'Meta Label']) !!}
+            {!! Form::text('meta_description', null, ['placeholder' => 'Meta Description']) !!}
+        </p>
+        <p>
+    {!! Form::submit((isset($item) ? 'Update' : 'Add') . ' Item') !!}
+        </p>
     {!! Form::close() !!}
+    </div>
 @endsection
